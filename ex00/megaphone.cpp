012a8fc379b16055861c1dd736f64c23574e7253
Joao Lucas soares <jlucas-s@c3r5p4.42sp.org.br>
2023-08-21 18:54:38 -0300

    initial commit02

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlucas-s <jlucas-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 15:45:27 by jlucas-s          #+#    #+#             */
+/*   Updated: 2023/08/21 16:18:44 by jlucas-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return (0);
+    }
+    for (int i = 1; argv[i]; i++) {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)std::toupper(argv[i][j]);
+    }
+    std::cout << std::endl;
+
+    return (0);
+}
