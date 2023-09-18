2ad27f31cacd124b3ee4a02b54e31113b3815bbf
Joao Lucas Soares <joaolucassoaresk@outlook.com>
2023-09-18 18:58:51 -0300

    max_contacts change for 8

--- CPP_00/ex01/classes/includes/PhoneBook.hpp
+++ CPP_00/ex01/classes/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: jlucas-s <jlucas-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:49:27 by jlucas-s          #+#    #+#             */
-/*   Updated: 2023/08/31 21:15:13 by jlucas-s         ###   ########.fr       */
+/*   Updated: 2023/09/18 18:56:28 by jlucas-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 #include "Contact.hpp"
 
-# define MAX_CONTACTS 2
+# define MAX_CONTACTS 8
 
 class PhoneBook {
 	private:
