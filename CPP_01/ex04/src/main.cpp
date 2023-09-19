f6e4f1f2f918b4918ec26f7db078521dbf484168
Joao Lucas Soares <joaolucassoaresk@outlook.com>
2023-09-19 14:56:41 -0300

    ex04 completed

--- CPP_01/ex04/src/main.cpp
+++ CPP_01/ex04/src/main.cpp
@@ -6,17 +6,28 @@
 /*   By: jlucas-s <jlucas-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:25:46 by jlucas-s          #+#    #+#             */
-/*   Updated: 2023/09/18 13:43:37 by jlucas-s         ###   ########.fr       */
+/*   Updated: 2023/09/19 14:54:30 by jlucas-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 
-// char * ft_replace(const std::string s1, const std::string s2) {
-    
-// }
+void ft_replace(std::string &line, const std::string s1, const std::string s2) {
+    int pos;
+
+    while (true) {
+        pos = line.find(s1);
+        if (pos == -1)
+            break;
+        else {
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -26,16 +37,18 @@ int main(int argc, char* argv[]) {
     }
 
     std::ifstream file(argv[1]);
-    std::ofstream newFile(".replace");
+    if (file.fail()) {
+        std::cout   << "Error!\n"
+                    << "Invalid file" << std::endl;
+        return (2);
+    }
+    std::ofstream newFile((std::string(argv[1]) + ".replace").c_str());
 
     std::string buffer;
-
     while (getline(file, buffer)) {
-        std::cout << buffer << std::endl; // teste
+        ft_replace(buffer, argv[2], argv[3]);
         newFile << buffer << std::endl;
     }
-
-
 
     file.close();
     newFile.close();
