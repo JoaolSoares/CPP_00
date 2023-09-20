1e8f1fc17df18128ddfd4d996ce76d669641ccfd
João Lucas Soares <joaolucassoaresk@outlook.com>
2023-09-20 20:56:08 -0300

    Update HumanB.cpp

--- CPP_01/ex03/classes/HumanB.cpp
+++ CPP_01/ex03/classes/HumanB.cpp
@@ -15,7 +15,8 @@
 HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
 
 void	HumanB::attack(void) const {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (this->_weapon != NULL)
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &weapon)	{ this->_weapon = &weapon;	}
