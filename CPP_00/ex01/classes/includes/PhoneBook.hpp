4620c3c89406e26c121488a93389282c5eb7bb70
João Lucas Soares <joaolucassoaresk@outlook.com>
2023-10-17 23:01:32 -0300

    Revert "CPP_03 finish"

--- CPP_00/ex01/classes/includes/PhoneBook.hpp
+++ CPP_00/ex01/classes/includes/PhoneBook.hpp
@@ -1,34 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlucas-s <jlucas-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 22:02:16 by jlucas-s          #+#    #+#             */
-/*   Updated: 2023/10/05 02:16:21 by jlucas-s         ###   ########.fr       */
+/*   Created: 2023/08/21 18:49:27 by jlucas-s          #+#    #+#             */
+/*   Updated: 2023/09/18 18:56:28 by jlucas-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef SCAVTRAP_HPP
-# define SCAVTRAP_HPP
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
 
-#include "ClapTrap.hpp"
 #include <iostream>
+#include "Contact.hpp"
 
-class ScavTrap: public ClapTrap {
+# define MAX_CONTACTS 8
+
+class PhoneBook {
 	private:
+		Contact		contacts[MAX_CONTACTS];
+		int			num_contacts;
+		int			oldest;
+		std::string	formatForList(std::string str);
 
 	public:
-		ScavTrap();
-		ScavTrap(std::string name);
-		ScavTrap(const ScavTrap &src);
-		~ScavTrap();
-
-		ScavTrap &operator = (const ScavTrap &obj);
-
-		void attack(const std::string& target);
-		void guardGate();
+				PhoneBook(void);
+		void	listContacts(void);
+		Contact getContact(int index);
+		int		getNumContacts();
+		void	addContact(Contact new_contact);
 };
 
-# endif
+#endif
